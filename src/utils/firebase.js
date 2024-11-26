06b51c05a90fca8331d6f96c33342f62f8b187f3
zoelaventhol <110328502+zoelaventhol@users.noreply.github.com>
2024-11-26 11:25:03 +0100

    reset config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,15 +4,14 @@ import { getDatabase, ref, set, get, child } from "firebase/database";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmAVvW60ypN8PQv7Rgf_LeI05RkOICME8",
-  authDomain: "gigshub-ff21e.firebaseapp.com",
-  projectId: "gigshub-ff21e",
-  storageBucket: "gigshub-ff21e.firebasestorage.app",
-  databaseURL:
-    "https://gigshub-ff21e-default-rtdb.europe-west1.firebasedatabase.app",
-  messagingSenderId: "390721411415",
-  appId: "1:390721411415:web:1452cdfe1b079b6f544612",
-  measurementId: "G-1WKWMLX4SP",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  databaseURL: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
