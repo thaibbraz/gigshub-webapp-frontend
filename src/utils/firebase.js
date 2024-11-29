0f7a04a22de1db47a4619366d732d8d6726267b8
zoelaventhol <110328502+zoelaventhol@users.noreply.github.com>
2024-11-29 18:41:07 +0100

    chore: back button and delete skills

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -26,74 +26,84 @@ const provider = new GoogleAuthProvider();
 const database = getDatabase(app);
 
 async function checkUserExists(userId) {
-    try {
-        console.log("Checking user with ID:", userId); // Debug log
-        const dbRef = ref(database); // Reference to the database
-        console.log("Database reference:", dbRef);
-        const snapshot = await get(child(dbRef, `users/${userId}`)); // Path to the user node
-        
-        console.log("Snapshot fetched:", snapshot); // Log the snapshot object
-    
-        if (snapshot.exists()) {
-            console.log("User exists:", snapshot.val());
-            return true;
-        } else {
-            console.log("User does not exist or no users node in the database.");
-            return false;
-        }
-        } catch (error) {
-        console.error("Error checking user:", error);
-        return false;
-        }
+  try {
+    console.log("Checking user with ID:", userId); // Debug log
+    const dbRef = ref(database); // Reference to the database
+    console.log("Database reference:", dbRef);
+    const snapshot = await get(child(dbRef, `users/${userId}`)); // Path to the user node
+
+    console.log("Snapshot fetched:", snapshot); // Log the snapshot object
+
+    if (snapshot.exists()) {
+      console.log("User exists:", snapshot.val());
+      return true;
+    } else {
+      console.log("User does not exist or no users node in the database.");
+      return false;
+    }
+  } catch (error) {
+    console.error("Error checking user:", error);
+    return false;
+  }
 }
 
-
 const addUserData = async (userId, data) => {
-    try {
-        if (!userId) throw new Error("User ID is undefined");
-        if (!data || typeof data !== "object") throw new Error("Invalid data");
-        
-        // Sanitize the data to remove any undefined or circular references
-        const sanitizedData = JSON.parse(JSON.stringify(data));
+  try {
+    if (!userId) throw new Error("User ID is undefined");
+    if (!data || typeof data !== "object") throw new Error("Invalid data");
 
-        // Reference to the user's data in the database
-        const userRef = ref(database, `users/${userId}`);
-        
-        // Check if the user already exists in the database
-        const snapshot = await get(userRef);
-        if (snapshot.exists()) {
-            // If user exists, returns
-            return;
-        }
+    // Sanitize the data to remove any undefined or circular references
+    const sanitizedData = JSON.parse(JSON.stringify(data));
 
-        // If user does not exist, create new user data
-        await set(userRef, sanitizedData);
-        console.log("User data successfully added.");
-    } catch (error) {
-        console.error("Error adding or updating user data:", error);
+    // Reference to the user's data in the database
+    const userRef = ref(database, `users/${userId}`);
+
+    // Check if the user already exists in the database
+    const snapshot = await get(userRef);
+    if (snapshot.exists()) {
+      // If user exists, returns
+      return;
     }
+
+    // If user does not exist, create new user data
+    await set(userRef, sanitizedData);
+    console.log("User data successfully added.");
+  } catch (error) {
+    console.error("Error adding or updating user data:", error);
+  }
 };
 
 const getUserCVData = async (userId) => {
   try {
-      if (!userId) throw new Error("User ID is undefined");
+    if (!userId) throw new Error("User ID is undefined");
 
-      // Reference to the CV data in the database
-      const cvRef = ref(database, `users/${userId}/cv`);
+    // Reference to the CV data in the database
+    const cvRef = ref(database, `users/${userId}/cv`);
 
-      // Fetch the CV data
-      const snapshot = await get(cvRef);
-      if (snapshot.exists()) {
-          console.log("CV data fetched:", snapshot.val());
-          return snapshot.val();
-      } else {
-          console.log("No CV data found for this user.");
-          return null;
-      }
-  } catch (error) {
-      console.error("Error fetching CV data:", error);
+    // Fetch the CV data
+    const snapshot = await get(cvRef);
+    if (snapshot.exists()) {
+      console.log("CV data fetched:", snapshot.val());
+      return snapshot.val();
+    } else {
+      console.log("No CV data found for this user.");
       return null;
+    }
+  } catch (error) {
+    console.error("Error fetching CV data:", error);
+    return null;
   }
 };
 
-export { database, ref, set, get, auth, provider, addUserData, checkUserExists, signInWithPopup, getUserCVData };
+export {
+  database,
+  ref,
+  set,
+  get,
+  auth,
+  provider,
+  addUserData,
+  checkUserExists,
+  signInWithPopup,
+  getUserCVData,
+};
