b3bf32cf3736d6fb0034636a657d5ed540447d7e
Thiago Braz <thiagobbraz@MacBook-Pro-de-Thiago.local>
2024-12-03 21:11:20 +0100

    feat: added matching score dyamic - part 1 - improved style and added more to the functionality of job-match

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -14,6 +14,8 @@ const Analytics = () => {
     const [showExperienceModal, setShowExperienceModal] = useState(false); 
     const [selectedCompanies, setSelectedCompanies] = useState("");
     const [optimizationSuggestion, setOptimizationSuggestion] = useState("");
+    const [highlightedSkills, setHighlightedSkills] = useState([]);
+
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem("user"));
         const userId = user?.uid;
@@ -61,7 +63,8 @@ const Analytics = () => {
 
         // Add the skill to cvData.skills.list
         const updatedSkills = [...cvData.skills[0].list, skill];
-
+        
+        setHighlightedSkills((prev) => [...prev, skill]);
         // Update state
         setAnalysisData((prev) => ({
             ...prev,
@@ -121,51 +124,90 @@ const Analytics = () => {
         education = [],
         skills = [],
     } = cvData;
+    const strokeColor =
+    matching_score < 30
+      ? "#E74C3C"
+      : matching_score > 65
+      ? "#27AE60"
+      : "#F2994A";
 
 
-    const handleExperienceAdd = async () => {
-        
-        if (selectedCompanies.length === 0) {
-            alert("Please select at least one company and provide a description.");
-            return;
-        }
-        let experience_worked = selectedCompanies.map((company) => ({...cvData.experiences.find((exp) => exp.company === company.value.company)}))
-
-        const payload = {
-            experience_gap: optimizationSuggestion,
-            exp_description: experience_worked[0].description
-            
-        };
-
-        try {
-            const response = await sendRequest(payload, "/add-experience");
-            setCvData((prevState) => ({
-                ...prevState,
-                experiences: prevState.experiences.map((exp) =>
-                    exp.company === selectedCompanies[0].value.company && selectedCompanies[0].value.title === exp.title
-                        ? { ...exp, description: response.resume_data.experience_updated }
-                        : exp
-                ),
+        const handleExperienceAdd = async () => {
+            if (selectedCompanies.length === 0) {
+                alert("Please select at least one company and provide a description.");
+                return;
+            }
+            const experienceWorked = selectedCompanies.map((company) => ({
+                ...cvData.experiences.find((exp) => exp.company === company.value.company),
             }));
+        
+            const payload = {
+                experience_gap: optimizationSuggestion,
+                exp_description: experienceWorked[0].description,
+            };
+        
+            try {
+                const response = await sendRequest(payload, "/add-experience");
+                const updatedDescription = response.resume_data.experience_updated;
+        
+                setCvData((prevState) => ({
+                    ...prevState,
+                    experiences: prevState.experiences.map((exp) =>
+                        exp.company === selectedCompanies[0].value.company && selectedCompanies[0].value.title === exp.title
+                            ? {
+                                ...exp,
+                                description: highlightAddedText(exp.description, updatedDescription),
+                            }
+                            : exp
+                    ),
+                }));
+        
+                console.log("Experience added successfully:", response);
+                alert("Experiences added successfully!");
+                toggleExperienceModal();
+            } catch (error) {
+                console.error("Error adding experiences:", error);
+                alert("Failed to add experiences.");
+            }
+            setJobDescription("");
+            setOptimizationSuggestion("");
+            setSelectedCompanies([]);
+        };
+        
+        // Helper Function to Highlight Added Text
+        const highlightAddedText = (originalText, updatedText) => {
+            if (!originalText || !updatedText) return updatedText;
+        
+            // Find the new text by comparing the updated text with the original
+            const newText = updatedText.replace(originalText, "");
             
-            console.log("Experience added successfully:", response);
+            // Highlight the new text
+            return updatedText.replace(newText, `<span class="highlight">${newText}</span>`);
+        };
+        const handleRecruitersTips = (value) => {
+            if (value === false) {
+                setCvData((prevCvData) => ({
+                    ...prevCvData,
+                    job_summary: analysisData.user_summary, 
+                }));
+            }
+        };
+        const handleJobTitle = (job_title_match) => {
+            console.log("Job title match:", job_title_match,job_title_match.length, typeof job_title_match);
             
-            alert("Experiences added successfully!");
-            toggleExperienceModal();
-        } catch (error) {
-            console.error("Error adding experiences:", error);
-            alert("Failed to add experiences.");
-        }
-        setJobDescription("");
-        setOptimizationSuggestion("");
-        setSelectedCompanies([]);
-    };
+            if (job_title_match?.length > 0) {
+                setCvData((prevCvData) => ({
+                    ...prevCvData,
+                    jobTitle: analysisData.job_title_match, 
+                }));
+            }
+        };
+        
+        
     return (
         <div className="App">
             <div className="container">
                 <div className="sidebar">
-                
-
                     <div className="btn-tailor-job-box">
                         <button className="upload-cv-btn-ghost" onClick={togglePopup}>
                             <span>Tailor cv to a job</span>
@@ -181,18 +223,68 @@ const Analytics = () => {
             {matching_score !== undefined && (
                 <div className="matching-score">
                     <h2>Matching Score</h2>
-                    <div className="score-circle">
-                        <span>{matching_score}</span>
-                    </div>
+                    <svg
+                        width="80"
+                        height="80"
+                        viewBox="0 0 36 36"
+                        className="score-circle"
+                    >
+                        {/* Background circle */}
+                        <circle
+                        cx="18"
+                        cy="18"
+                        r="16"
+                        stroke="#E0E0E0"
+                        strokeWidth="4"
+                        fill="none"
+                        ></circle>
+
+                        {/* Dynamic progress circle */}
+                        <circle
+                        cx="18"
+                        cy="18"
+                        r="16"
+                        strokeDasharray="100"
+                        strokeDashoffset={100 - matching_score}
+                        stroke={strokeColor}
+                        strokeWidth="4"
+                        fill="none"
+                        strokeLinecap="round"
+                        ></circle>
+
+                        {/* Text in the middle */}
+                        <text
+                        x="50%"
+                        y="50%"
+                        textAnchor="middle"
+                        fill={strokeColor}
+                        fontSize="8px"
+                        dy=".3em"
+                        >
+                        {matching_score}
+                        </text>
+                    </svg>
                     <p>{summary_of_issues?.total_issues_count || 0} issues found</p>
                 </div>
             )}
 
-            {/* Job Title Match */}
-            {job_title_match && (
+            {/* Job Title Match 
+            
+            <button className="match-btn" onClick={() => toggleExperienceModal(job_title_match)}> - Apply
+                            </button>
+                            */}
+            {job_title_match && job_title_match?.length > 0 && (
                 <div className="section">
                     <h3>Job Title Match</h3>
-                    <p>{job_title_match}</p>
+                        <div className="flex">
+                            <p>{job_title_match}</p>
+                            <button className="experence-btn ml-2 mt-1" onClick={() => handleJobTitle(job_title_match)}>Add
+                                <img
+                                src={starsUnfilled}
+                                alt="Stars Icon"
+                                className="w-2 h-2"
+                                /></button>
+                        </div>
                 </div>
             )}
 
@@ -202,8 +294,11 @@ const Analytics = () => {
                     <h3>Searchability</h3>
                     <ul>
                         {Object.entries(searchability).map(([key, value], index) => (
-                            <li key={index}>
-                                {value ? "✔" : "⚠"} {key.replace(/_/g, " ")}
+                            <li key={index} >
+                                {key.replace(/_/g, " ")} 
+                                <span className="ml-2" style={{ color: value ? "#13BC4B" : "#FF1212" }}>
+                                    {value ? "Included" : "Missing"}
+                                </span>
                             </li>
                         ))}
                     </ul>
@@ -238,7 +333,7 @@ const Analytics = () => {
                         experience_gaps.unrepresented_roles.map((role, index) => (
                             <div className="flex">
                                 <li className="mr-2" key={index}> {role}</li>
-                                <button className="experence-btn" onClick={() => toggleExperienceModal(role)}>Add to experience
+                                <button className="experence-btn" onClick={() => toggleExperienceModal(role)}>Add
                                 <img
                                 src={starsUnfilled}
                                 alt="Stars Icon"
@@ -248,8 +343,8 @@ const Analytics = () => {
                         ))} 
                     </ul>
                 </div>
-            )}*/}
-
+            )}
+        */}
             {/* Education Requirements */}
             {education_requirements?.missing_degrees_or_certifications?.length > 0 && (
                 <div className="section">
@@ -257,7 +352,7 @@ const Analytics = () => {
                     <ul>
                         {education_requirements.missing_degrees_or_certifications.map(
                             (degree, index) => (
-                                <li key={index}>⚠ {degree}</li>
+                                <li key={index}> {degree}</li>
                             )
                         )}
                     </ul>
@@ -271,7 +366,7 @@ const Analytics = () => {
                     <ul>
                         {optimization_suggestions.map((suggestion, index) => (
                             <div className="flex">
-                                <li key={index}>✔ {suggestion}</li>
+                                <li key={index}>{suggestion}</li>
                                 <button className="experence-btn" onClick={() => toggleExperienceModal(suggestion)}>Add
                                 <img
                                 src={starsUnfilled}
@@ -291,9 +386,15 @@ const Analytics = () => {
                     <h3>Recruiters' Tips</h3>
                     <ul>
                         {Object.entries(recruiters_tips).map(([key, value], index) => (
-                            <li key={index}>
+                            <div className="flex"><li key={index}>
                                 {value ? "✔" : "⚠"} {key.replace(/_/g, " ")}
-                            </li>
+                            </li><button className="ml-2 experence-btn" onClick={() => handleRecruitersTips(value)}>Add
+                                <img
+                                src={starsUnfilled}
+                                alt="Stars Icon"
+                                className="ml-1 mr-1 w-2 h-2"
+                                /></button>
+                            </div>
                         ))}
                     </ul>
                 </div>
@@ -425,6 +526,7 @@ const Analytics = () => {
                         <div id="resumePreview">
                             <h2 id="previewName">{`${cvData["first name"]} ${cvData["last name"]}`}</h2>
                             <p id="previewJobTitle">{jobTitle}</p>
+                            <p id="previewJobSummary" className="pb-2">{cvData?.job_summary ? cvData.job_summary : ""}</p>
                             <div className="preview-section">
                                 <h3>Contact Information</h3>
                                 <p id="previewEmail">{email}</p>
@@ -441,7 +543,7 @@ const Analytics = () => {
                                             <strong>{exp.title}</strong> <span>{exp.date === "undefined - undefined" ? "" : exp.date}</span>
                                         </p>
                                         <p><em>{exp.company}</em></p>
-                                        <p>{exp.description}</p>
+                                        <p dangerouslySetInnerHTML={{ __html: exp.description }}></p>
                                     </div>
                                 ))}
                             </div>
@@ -456,8 +558,12 @@ const Analytics = () => {
                             <div className="preview-section">
                                 <h3>Skills</h3>
                                 <ul>
-                                    {skills?.[0]?.list.map((skill, index) => (
-                                        <li key={index}>{skill}</li>
+                                    {cvData?.skills?.[0]?.list.map((skill, index) => (
+                                        <li key={index}>
+                                            <span className={highlightedSkills.includes(skill) ? "highlight" : ""}>
+                                                {skill}
+                                            </span>
+                                        </li>
                                     ))}
                                 </ul>
                             </div>
