31e810b8b001e0ee907fd5a9125d31d91dc55e3e
zoelaventhol <110328502+zoelaventhol@users.noreply.github.com>
2024-12-10 10:03:24 +0100

    chore: logic to show cached jobs first, then fetch jobs

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const BASE_URL =
- "https://fastapi-service-03-160893319817.europe-southwest1.run.app";
- // "http://localhost:8000";
+  "https://fastapi-service-03-160893319817.europe-southwest1.run.app";
+// "http://localhost:8000";
+
+const JOBS_URL = "http://127.0.0.1:8001/jobs";
+
 export async function sendRequest(data, endpoint) {
   try {
     const token = localStorage.getItem("token");
@@ -19,7 +22,22 @@ export async function sendRequest(data, endpoint) {
     ) {
       const token = response.data.access_token;
       localStorage.setItem("token", token);
-    }else
+    } else return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function sendJobsRequest(data) {
+  console.log("api.js payload: ", data);
+  try {
+    const token = localStorage.getItem("token");
+    let options = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    let response = await axios.post(`${JOBS_URL}`, data, options);
     return response.data;
   } catch (error) {
     throw new Error(error);
